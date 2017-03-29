d27a630ef2583de67c00de893c510d57e0ff98cc
workline521 <workline521@gmail.com>
2017-03-29 13:49:36 +0400

    Optimisation for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,33 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
+
+gulp.task("images", function(){
+	return gulp.src("img/**/*.{png,jpg,gif}")
+	  .pipe(imagemin([
+		imagemin.optipng({optimizationLevel: 3}),
+		imagemin.jpegtran({progressive: true})
+	]))
+	.pipe(gulp.dest("img"));
+});
+
+gulp.task("sprite", function() {
+	return gulp.src("img/*.svg")
+	  .pipe(svgmin())
+	  .pipe(svgstore({
+		inlineSvg: true
+	  }))
+	  .pipe(rename("sprite.svg"))
+	  .pipe(gulp.dest("img"));
+});
+
+
 gulp.task("style", function() {
   gulp.src("less/style.less")
     .pipe(plumber())
@@ -14,9 +41,15 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer({browsers: [
         "last 2 versions"
-      ]})
-    ]))
+      ]}),
+	  mqpacker({
+		  sort: true
+	  })
+	]))
     .pipe(gulp.dest("css"))
+	.pipe(minify())
+	.pipe(rename("style.min.css"))
+	.pipe(gulp.dest("css"))
     .pipe(server.stream());
 });
 
