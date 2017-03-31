e0651f5057e25f0ece85f42a833ee652fda40078
workline521 <workline521@gmail.com>
2017-03-31 18:03:32 +0400

    HTML edit according to checklist_29_03_2017

--- js/pink.js
+++ js/pink.js
@@ -1,19 +1,22 @@
-var headerMenu = document.querySelector('.header-menu');
-		var navToggle = document.querySelector('.header-stripe__toggle');
-		var headerStripe = document.querySelector('.header-stripe');
-		navToggle.addEventListener('click', function () {
-			if (headerMenu.classList.contains('header-menu--closed')) {
-				navToggle.classList.add('header-stripe__toggle--opened')
-				navToggle.classList.remove('header-stripe__toggle--closed')
-				headerMenu.classList.remove('header-menu--closed');
-				headerMenu.classList.add('header-menu__toggle--opened');
-				headerStripe.classList.add('header-stripe--bg');
-			}
-			else {
-				headerMenu.classList.add('header-menu--closed');
-				headerMenu.classList.remove('header-menu--opened');
-				navToggle.classList.remove('header-stripe__toggle--opened');
-				navToggle.classList.add('header-stripe__toggle--closed');
-				headerStripe.classList.remove('header-stripe--bg');
-			}
-		});
+        var headerMenu = document.querySelector('.header-menu');
+        var navToggle = document.querySelector('.header-menu__toggle');
+        var headerStripe = document.querySelector('.header-stripe');
+        headerMenu.classList.remove('header-menu--nojs');
+        if (headerMenu.classList.contains('header-menu--nojs')) {
+        	headerStripe.classList.add('header-stripe--bg');
+        }
+        else {
+        	headerStripe.classList.remove('header-stripe--bg');
+        }
+        navToggle.addEventListener('click', function () {
+        	if (headerMenu.classList.contains('header-menu--closed')) {
+        		headerMenu.classList.remove('header-menu--closed');
+        		headerMenu.classList.add('header-menu--opened');
+        		headerStripe.classList.add('header-stripe--bg');
+        	}
+        	else {
+        		headerMenu.classList.remove('header-menu--opened');
+        		headerMenu.classList.add('header-menu--closed');
+        		headerStripe.classList.remove('header-stripe--bg');
+        	}
+        });
