37b0a3755a541629f142b1d338d10ac41f8ef89c
workline521 <workline521@gmail.com>
2017-04-07 12:58:10 +0400

    Тест на переполнение контентом

--- js/pink.js
+++ js/pink.js
@@ -1,10 +1,14 @@
-
+var formSubmit = document.querySelector('.form__submit');
+var formSuccess = document.querySelector(".form-succes");
+var formFail = document.querySelector(".form-fail");
+var successClose = document.querySelector(".form-success__close");
+var failClose = document.querySelector(".form-fail__ok");
 var headerMenu = document.querySelector('.header-menu');
 var navToggle = document.querySelector('.header-menu__toggle');
 var headerStripe = document.querySelector('.header-stripe');
-
+var form = document.querySelector(".form");
 headerMenu.classList.remove('header-menu--nojs');
-headerStripe.classList.remove('header-stripe--nojs')
+headerStripe.classList.remove('header-stripe--nojs');
 if (headerMenu.classList.contains('header-menu--nojs')) {
   headerStripe.classList.add('header-stripe--bg');
 }
@@ -22,4 +26,26 @@ navToggle.addEventListener('click', function () {
     headerMenu.classList.add('header-menu--closed');
     headerStripe.classList.remove('header-stripe--bg');
   }
-})
+});
+formSubmit.addEventListener("click", function (event) {
+  event.preventDefault();
+  formSuccess.classList.add("form-success--show");
+});
+successClose.addEventListener("click", function (event) {
+  formSuccess.classList.remove("form-success--show");
+});
+ window.addEventListener("keydown", function(event) {
+        if (event.keyCode === 27) {
+          if (formSuccess.classList.contains("form-success--show")) {
+            formSuccess.classList.remove("form-success--show");
+          }
+        }
+      });
+
+
+
+
+
+
+
+
